4e22610410d4256b3cdc37921b9035e0933928cb
rtv313isc <rtv313.isc@hotmail.com>
2020-04-03 12:36:28 -0600

    Partition 2_4 done

---
+++ src/Chapter2/Partition2_4.java
@@ -0,0 +1,46 @@
+package Chapter2;
+
+public class Partition2_4 {
+
+    public static void partition(LinkedList list,int x){
+        LinkedList belowX = new LinkedList();
+        LinkedList equalX = new LinkedList();
+        LinkedList aboveX = new LinkedList();
+        Node runner = list.head;
+
+        while(runner != null){
+            if(runner.data < x){
+                belowX.addNode(runner.data);
+            }
+            if(runner.data > x){
+                aboveX.addNode(runner.data);
+            }
+            if(runner.data == x){
+                equalX.addNode(runner.data);
+            }
+            runner = runner.next;
+        }
+
+        belowX.tail.next = equalX.head;
+        equalX.tail.next = aboveX.head;
+        list.head = belowX.head;
+    }
+
+    public static void main(String args[]){
+        LinkedList list  = new LinkedList();
+        list.addNode(10);
+        list.addNode(9);
+        list.addNode(8);
+        list.addNode(7);
+        list.addNode(6);
+        list.addNode(5);
+        list.addNode(4);
+        list.addNode(3);
+        list.addNode(2);
+        list.addNode(1);
+
+        partition(list,5);
+        list.displayList();
+
+    }
+}
